ce8f9a9d880c777b9f253d44eb9fa971ebe8f7f4
Jack Lingle <captainjack0818@gmail.com>
2023-04-11 19:21:20 -0400

    Add renderAbout function to about.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import renderHome from './home';
 import renderMenu from './menu';
+import renderAbout from './about';
 
 const content = document.getElementById('content');
 content.innerHTML = '';
-renderMenu();
+renderAbout();
